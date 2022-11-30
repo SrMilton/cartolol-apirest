2694bda10b9ca315c7f1b049af4b567bbb9b4acd
Milton Ramos <miltinhulindinhu@gmail.com>
2022-11-30 01:44:07 -0300

    Add Ranking

---
+++ pages/api/get_ranking.ts
@@ -0,0 +1,22 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import NextCors from 'nextjs-cors';
+import {executeQuery} from '../../config/db'
+
+export default async function handler(req: NextApiRequest,res: NextApiResponse)
+{
+  await NextCors(req, res, {
+    // Options
+    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+    origin: '*',
+    optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+ });
+
+ var query = "SELECT id, username, email, ptos FROM users WHERE ptos > 0 ORDER BY ptos DESC;"
+ var checkuser = await executeQuery(query, [])
+
+ return res.status(200).json(
+    { status: 'true',
+    ranking: checkuser}
+    )
+
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -246,6 +246,29 @@ export default function Home() {
         1 - status: false | msng: Ocorreu um erro interno, tente novamente <br></br>
         </div>
 
+        <div className={styles.description}>
+        <h2 >
+          Get Ranking
+        </h2>
+        
+          
+        URL: <code className={styles.code}>https://cartolol-apirest.vercel.app/api/get_ranking</code>
+          <br></br>
+        
+        <h4 >
+          Possiveis retornos:
+        </h4>
+        <h5 >
+          Status Code 200:
+        </h5>
+        1 - status: true | ranking: [array] <br></br>
+
+        <h5 >
+          Status Code 500:
+        </h5>
+        1 - status: false | msng: Ocorreu um erro interno, tente novamente <br></br>
+        </div>
+
         
       </main>
     </div>
