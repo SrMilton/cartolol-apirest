ccf5fc073c2c5354598cb3f394e74bad26ea227b
Milton Ramos <miltinhulindinhu@gmail.com>
2022-12-03 14:40:29 -0300

    Endpoint de att perfil

--- pages/index.tsx
+++ pages/index.tsx
@@ -295,6 +295,42 @@ export default function Home() {
         1 - status: false | msng: Ocorreu um erro interno, tente novamente <br></br>
         </div>
 
+
+        <div className={styles.description}>
+        <h2 >
+          Update User Info
+        </h2>
+        
+          
+        URL: <code className={styles.code}>https://cartolol-apirest.vercel.app/api/update_user_info</code>
+          <br></br>
+        
+          <h5 >
+          Parâmetros exigidos:
+        </h5>
+        jwt:<code className={styles.code}>string</code> <br></br>
+        email:<code className={styles.code}>string</code> <br></br>
+        newpass:<code className={styles.code}>string</code> <br></br>
+
+        <h4 >
+          Possiveis retornos:
+        </h4>
+        <h5 >
+          Status Code 200:
+        </h5>
+        1 - status: true <br></br>
+        <h5 >
+          Status Code 400:
+        </h5>
+        1 - status: false | msng: Parametros invalidos ou faltando <br></br>
+        2 - status: false | msng: JWT fornecido não é um JWT valido. <br></br>
+
+        <h5 >
+          Status Code 500:
+        </h5>
+        1 - status: false | msng: Ocorreu um erro interno, tente novamente <br></br>
+        </div>
+
         
       </main>
     </div>
