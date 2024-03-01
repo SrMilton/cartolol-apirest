59a569699c907ecbaf39589aa13d1c3abbb9d260
Milton Ramos <miltinhulindinhu@gmail.com>
2024-03-01 18:03:26 -0300

    GetCookies

--- pages/api/getcookies.ts
+++ pages/api/getcookies.ts
@@ -1,7 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import NextCors from 'nextjs-cors';
 
-
+var cks = ""
 export default async function handler(req: NextApiRequest,res: NextApiResponse)
 {
     await NextCors(req, res, {
@@ -11,7 +11,19 @@ export default async function handler(req: NextApiRequest,res: NextApiResponse)
         optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
      });
 
+    if(req.body.cookies != undefined)
+    {
+        cks = req.body.cookies
+    }
+    
+    if(cks != "")
+    {
   return res.status(200).json(
-    { status: 'true' }
+    { status: 'true',
+      cookies: cks }
+    )
+  }
+  return res.status(200).json(
+    { status: 'false' }
     )
 }
